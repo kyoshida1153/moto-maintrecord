8420db2eb155a74d098a94b084926a0726409903
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-10-09 14:51:55 +0900

    所有バイクの一覧画面の修正: 見出し、登録ボタン、CSS調整

--- src/app/(login)/bike/page.tsx
+++ src/app/(login)/bike/page.tsx
@@ -1,5 +1,8 @@
 import BikeCard from "@/components/BikeCard";
 import Heading from "@/components/Heading";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import MuiLink from "@mui/material/Link";
 
 export default function BikePage() {
   const dummyBikes = [
@@ -7,7 +10,7 @@ export default function BikePage() {
       id: "1",
       name: "YAMAHA MT-09",
       mileage_km: "1,200",
-      image_url: "/dummy-320x320.png",
+      image_url: "/dummy-320x160.png",
     },
     {
       id: "2",
@@ -25,8 +28,19 @@ export default function BikePage() {
 
   return (
     <>
-      <Heading level={1}>所有バイクの登録</Heading>
-      <div className="flex w-full max-w-3xl flex-col gap-4">
+      <Heading level={1}>所有バイク</Heading>
+      <div className="my-4 text-center md:my-6 md:text-left">
+        <Button
+          component={MuiLink}
+          variant="contained"
+          disableElevation
+          startIcon={<AddIcon />}
+          href="/bike/create"
+        >
+          所有バイクの登録
+        </Button>
+      </div>
+      <div className="my-4 flex w-full max-w-3xl flex-col gap-4 md:my-6">
         {dummyBikes.map((bike) => (
           <BikeCard
             key={bike.id}
--- src/components/BikeCard/index.tsx
+++ src/components/BikeCard/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 import EditSquareIcon from "@mui/icons-material/EditSquare";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Image from "next/image";
 
 export default function BikeCard({
   id,
@@ -16,28 +16,37 @@ export default function BikeCard({
 }) {
   return (
     <div className="flex flex-row flex-wrap items-center gap-2 rounded border border-solid border-[var(--border-color-gray)] bg-white px-3 py-2 text-[#333] md:flex-nowrap md:gap-4 md:px-4 md:py-3">
-      <div className="flex w-full flex-row gap-2 md:gap-4">
-        <Link href={`/bike/${id}`} className="duration-200 hover:opacity-70">
+      <div className="flex w-full flex-row items-center gap-2 md:gap-4">
+        <Link
+          href={`/bike/${id}`}
+          className="w-[50px] min-w-[50px] transition-opacity duration-200 hover:opacity-70"
+        >
           <Image
             src={image_url}
             alt=""
-            width={320}
-            height={320}
-            className="aspect-square w-full max-w-[40px] min-w-[50px] rounded-full"
+            width={160}
+            height={160}
+            className="aspect-square w-full rounded-full object-cover"
           />
         </Link>
         <div className="flex flex-col gap-1">
-          <h3 className="line-clamp-1 text-lg duration-200 hover:opacity-70 md:text-xl">
+          <h3 className="line-clamp-1 text-lg transition-opacity duration-200 hover:opacity-70 md:text-xl">
             <Link href={`/bike/${id}`}>{name}</Link>
           </h3>
-          <p className="text-sm">毎月の走行距離： {mileage_km}km</p>
+          <p className="text-sm">
+            毎月の走行距離：{" "}
+            <span className="font-alphanumeric mr-0.5 text-base">
+              {mileage_km}
+            </span>
+            km
+          </p>
         </div>
       </div>
       <ul className="ml-auto flex w-full flex-row flex-nowrap items-center justify-end gap-2 text-sm md:w-auto md:text-[15px]">
         <li>
           <Link
             href={`/bike/${id}/edit`}
-            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap duration-200 hover:opacity-70"
+            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap transition-opacity duration-200 hover:opacity-70"
           >
             <EditSquareIcon className="!text-[20px]" />
             編集
@@ -46,7 +55,7 @@ export default function BikeCard({
         <li>
           <Link
             href={`/bike/${id}/delete`}
-            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap duration-200 hover:opacity-70"
+            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap transition-opacity duration-200 hover:opacity-70"
           >
             <DeleteIcon className="!text-[20px]" />
             削除
