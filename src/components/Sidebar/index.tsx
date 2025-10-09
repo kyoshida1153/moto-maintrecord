d4df5dda6048b55a4be65c0c1fcb4072af775506
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-10-09 11:49:34 +0900

    共通レイアウトの変更: 調整

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import SidebarMainMenu from "./SidebarMainMenu";
 
 export default function Sidebar() {
   return (
-    <nav className="fixed h-full w-[220px] overflow-y-visible bg-[#fff] [border-right:1px_solid_#e9ecef]">
+    <nav className="fixed h-full w-[220px] overflow-y-visible bg-[#fff] [border-right:1px_solid_var(--border-color-gray)]">
       <div className="p-[16px]">
         <SidebarMainMenu />
       </div>
