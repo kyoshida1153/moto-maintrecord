e9b8086dd5d315c473157a2c484eec8c6731b8b5
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-10-11 20:53:28 +0900

    アカウントのメールアドレス変更画面の作成

---
+++ src/app/(login)/account/email/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/Heading";
+import AccountEmailEditForm from "@/components/AccountEmailEditForm";
+
+export default function AccountEmailEditPage() {
+  return (
+    <>
+      <Heading level={1}>メールアドレスの変更</Heading>
+      <AccountEmailEditForm />
+    </>
+  );
+}
