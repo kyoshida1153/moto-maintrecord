f7d176ba6eb530b11b3589b6e37d1b614b75be29
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-10-22 19:31:05 +0900

    Supabaseストレージを利用するための設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+if (!supabaseUrl) {
+  throw new Error("NEXT_PUBLIC_SUPABASE_URLが設定されていません。");
+}
+const supabaseUrlObject = new URL(supabaseUrl);
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: supabaseUrlObject.hostname,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
