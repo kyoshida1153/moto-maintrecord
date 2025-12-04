ba71486931842242bd74702d3841e5b444a5a9fc
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-12-04 12:08:24 +0900

    所有バイク/一覧機能の変更: カードに詳細リンクとメニュー追加

--- src/app/(login)/bike/_components/BikeCard/index.tsx
+++ src/app/(login)/bike/_components/BikeCard/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Prisma } from "@prisma/client";
+
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import EditSquareIcon from "@mui/icons-material/EditSquare";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Prisma } from "@prisma/client";
 
 export type Bike = Prisma.BikeGetPayload<{
   select: {
@@ -14,8 +20,17 @@ export type Bike = Prisma.BikeGetPayload<{
 }>;
 
 export default function BikeCard({ id, name, mileage, imageUrl }: Bike) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="flex flex-row flex-wrap items-center gap-2 rounded border border-solid border-[var(--border-color-gray)] bg-white px-3 py-2 text-[#333] md:flex-nowrap md:gap-4 md:px-4 md:py-3">
+    <div className="flex flex-row flex-nowrap items-center gap-2 rounded border border-solid border-[var(--border-color-gray)] bg-white px-3 py-2 text-[#333] md:gap-4 md:px-4 md:py-3">
       <div className="flex w-full flex-row items-center gap-2 md:gap-4">
         <span className="w-[50px] min-w-[50px]">
           <Image
@@ -27,7 +42,14 @@ export default function BikeCard({ id, name, mileage, imageUrl }: Bike) {
           />
         </span>
         <div className="flex flex-col gap-1">
-          <h2 className="line-clamp-1 text-lg md:text-xl">{name}</h2>
+          <h2 className="line-clamp-1 text-lg md:text-xl">
+            <Link
+              href={`/bike/${id}`}
+              className="text-[var(--link-color)] transition-opacity duration-200 hover:opacity-70"
+            >
+              {name}
+            </Link>
+          </h2>
           <p className="text-sm">
             毎月の走行距離：{" "}
             {typeof mileage === "number" ? (
@@ -43,26 +65,51 @@ export default function BikeCard({ id, name, mileage, imageUrl }: Bike) {
           </p>
         </div>
       </div>
-      <ul className="ml-auto flex w-full flex-row flex-nowrap items-center justify-end gap-3 text-sm md:w-auto md:text-[15px]">
-        <li>
-          <Link
-            href={`/bike/${id}/edit`}
-            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap transition-opacity duration-200 hover:opacity-70"
-          >
-            <EditSquareIcon sx={{ fontSize: "20px" }} />
-            編集
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/bike/${id}/delete`}
-            className="flex flex-row flex-nowrap items-center gap-[1px] whitespace-nowrap transition-opacity duration-200 hover:opacity-70"
-          >
-            <DeleteIcon sx={{ fontSize: "20px" }} />
-            削除
-          </Link>
-        </li>
-      </ul>
+      <div>
+        <IconButton
+          id="positioned-button"
+          aria-controls={open ? "positioned-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MoreVertIcon sx={{ fontSize: "24px" }} />
+        </IconButton>
+        <Menu
+          id="positioned-menu"
+          aria-labelledby="positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem>
+            <Link
+              href={`/bike/${id}/edit`}
+              className="flex items-center gap-1 text-[15px]"
+            >
+              <EditSquareIcon sx={{ fontSize: "18px" }} />
+              <span>編集する</span>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              href={`/bike/${id}/delete`}
+              className="flex items-center gap-1 text-[15px]"
+            >
+              <DeleteIcon sx={{ fontSize: "18px" }} />
+              <span>削除する</span>
+            </Link>
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
